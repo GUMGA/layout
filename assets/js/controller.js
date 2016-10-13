a932ff23d9c97f14fda623d1315bdde5774d84aa
Gui Seek <guiseek@gmail.com>
2016-10-13 16:39:24 -0300

    Melhorias no header e nav

---
+++ assets/js/controller.js
@@ -0,0 +1,33 @@
+angular
+  .module('app', ['gumga.layout'])
+  .controller('controller', controller)
+  
+  controller.$inject = ['$scope', '$http', '$timeout']
+  function controller($scope, $http, $timeout) {
+    $scope.navCollapse = function() {
+      document.querySelector('.gumga-layout > nav')
+        .classList.toggle('collapsed')
+    }
+    $scope.toggleSearch = function() {
+      document.querySelector('header > .searchbar')
+        .classList.toggle('searchShow')
+    }
+    
+    $http.get('assets/data/organizations.json')
+      .then(function(response) {
+        $scope.organizations = response.data
+      })
+    $timeout(function() {      
+      $http.get('assets/data/menu.json')
+        .then(function(response) {
+          $scope.menu = response.data
+        })
+    }, 1000)
+    $timeout(function() {      
+      $http.get('assets/data/keys.json')
+        .then(function(response) {
+          $scope.keys = response.data
+        })
+    }, 2000)
+    
+  }
---
+++ src/components/menu/component.js
@@ -0,0 +1,45 @@
+let Component = {
+  bindings: {
+    menu: '<',
+    keys: '<'
+  },
+  template: `
+    <ul>
+      <li ng-repeat="item in $ctrl.menu" ng-show="$ctrl.allow(item)" ng-class="{header: item.type == 'header', divider: item.type == 'separator'}">
+        <a ng-if="item.label != 'separator'">
+          <i ng-if="item.icon" class="material-icons" ng-bind="item.icon"></i>
+          <span ng-bind="item.label"></span>
+        </a>
+        <gl-menu ng-if="item.children.length > 0" menu="item.children"></gl-menu>
+      </li>
+    </ul>
+  `,
+  controller: function() {
+    let ctrl = this
+    ctrl.keys = ctrl.keys || []
+ 
+    // ctrl.allowByChildren = children => {
+    //   if (ctrl.keys.length > 0) {
+    //     if (children && children.length) {
+    //       return children.filter(child => {
+    //         return ctrl.keys.includes(child.key)
+    //       })
+    //     }
+    //   }
+    // }
+    
+    ctrl.allow = item => {
+      if (ctrl.keys.length > 0) {
+        return ctrl.keys.includes(item.key)
+        // if (!item.key && (item.children && item.children.length == 0)) return true
+        // if (item.key && (item.children && item.children.length == 0)) return ctrl.keys.includes(item.key)
+        // return ctrl.allowByChildren(item.children)
+        //   if (item.children && item.children.length == 0) 
+        //   if (item.children && item.children.length > 0) return item.children.reduce(child => ctrl.keys.includes(child.key))
+        // } 
+      }
+    }
+  }
+}
+
+export default Component
