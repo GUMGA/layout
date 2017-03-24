0e7ce86cd671bdb00678962867b13c3e2340f4ab
Mateus Miranda <info.mateusmiranda@gmail.com>
2017-03-24 10:37:12 -0300

    merge

--- dist/gumga-layout.js
+++ dist/gumga-layout.js
@@ -5,11 +5,67 @@ var _component = require('./menu/component.js');
 
 var _component2 = _interopRequireDefault(_component);
 
+var _component3 = require('./notification/component.js');
+
+var _component4 = _interopRequireDefault(_component3);
+
+var _component5 = require('./select/component.js');
+
+var _component6 = _interopRequireDefault(_component5);
+
+var _component7 = require('./input/component.js');
+
+var _component8 = _interopRequireDefault(_component7);
+
+var _component9 = require('./ripple/component.js');
+
+var _component10 = _interopRequireDefault(_component9);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-angular.module('gumga.layout', []).component('glMenu', _component2.default);
+angular.module('gumga.layout', []).component('glMenu', _component2.default).component('glNotification', _component4.default).component('gmdSelect', _component6.default).component('gmdInput', _component8.default).directive('gmdRipple', _component10.default);
 
-},{"./menu/component.js":2}],2:[function(require,module,exports){
+},{"./input/component.js":2,"./menu/component.js":3,"./notification/component.js":4,"./ripple/component.js":5,"./select/component.js":6}],2:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var Component = {
+  transclude: true,
+  bindings: {},
+  template: '\n    <div ng-transclude></div>\n  ',
+  controller: ['$scope', '$element', '$attrs', '$timeout', '$parse', function ($scope, $element, $attrs, $timeout, $parse) {
+    var ctrl = this,
+        input = void 0,
+        model = void 0;
+
+    var changeActive = function changeActive(target) {
+      if (target.value) {
+        target.classList.add('active');
+      } else {
+        target.classList.remove('active');
+      }
+    };
+    ctrl.$postLink = function () {
+      input = angular.element($element.find('input'));
+      model = input.attr('ng-model') || input.attr('data-ng-model');
+      $timeout(function () {
+        $scope.$parent.$watch(model, function (val) {
+          if (val != undefined) input[0].value = val;
+          changeActive(input[0]);
+        });
+      });
+      input.bind('blur', function (e) {
+        changeActive(e.target);
+      });
+    };
+  }]
+};
+
+exports.default = Component;
+
+},{}],3:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -20,7 +76,7 @@ var Component = {
     menu: '<',
     keys: '<'
   },
-  template: '\n    <ul>\n      <li data-ng-repeat="item in $ctrl.menu" data-ng-show="$ctrl.allow(item)" data-ng-class="{header: item.type == \'header\', divider: item.type == \'separator\'}">\n        <a ng-if="item.type != \'separator\'" ui-sref="item.state">\n          <i data-ng-if="item.icon" class="material-icons" data-ng-bind="item.icon"></i>\n          <span ng-bind="item.label"></span>\n        </a>\n        <gl-menu data-ng-if="item.children.length > 0" menu="item.children" keys="$ctrl.keys"></gl-menu>\n      </li>\n    </ul>\n  ',
+  template: '\n    <ul>\n      <li data-ng-repeat="item in $ctrl.menu" data-ng-show="$ctrl.allow(item)" data-ng-class="{header: item.type == \'header\', divider: item.type == \'separator\'}">\n        <a ng-if="item.type != \'separator\'" ui-sref="{{item.state}}">\n          <i data-ng-if="item.icon" class="material-icons" data-ng-bind="item.icon"></i>\n          <span ng-bind="item.label"></span>\n        </a>\n        <gl-menu data-ng-if="item.children.length > 0" menu="item.children" keys="$ctrl.keys"></gl-menu>\n      </li>\n    </ul>\n  ',
   controller: function controller() {
     var ctrl = this;
     ctrl.keys = ctrl.keys || [];
@@ -28,10 +84,135 @@ var Component = {
     ctrl.allow = function (item) {
       if (ctrl.keys.length > 0) {
         if (!item.key) return true;
-        return ctrl.keys.includes(item.key);
+        return ctrl.keys.indexOf(item.key) > -1;
       }
     };
   }
+};
+
+exports.default = Component;
+
+},{}],4:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var Component = {
+  bindings: {
+    icon: '@',
+    notifications: '=',
+    onView: '&?'
+  },
+  template: '\n    <ul class="nav navbar-nav navbar-right notifications">\n      <li class="dropdown">\n        <a href="#" badge="{{$ctrl.notifications.length}}" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">\n          <i class="material-icons" data-ng-bind="$ctrl.icon"></i>\n        </a>\n        <ul class="dropdown-menu">\n          <li data-ng-repeat="item in $ctrl.notifications" data-ng-click="$ctrl.view($event, item)">\n            <div class="media">\n              <div class="media-left">\n                <img class="media-object" data-ng-src="{{item.image}}">\n              </div>\n              <div class="media-body" data-ng-bind="item.content"></div>\n            </div>\n          </li>\n        </ul>\n      </li>\n    </ul>\n  ',
+  controller: function controller() {
+    var ctrl = this;
+    ctrl.view = function (event, item) {
+      return ctrl.onView({ event: event, item: item });
+    };
+  }
+};
+
+exports.default = Component;
+
+},{}],5:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var Component = function Component() {
+  return {
+    restrict: 'C',
+    link: function link($scope, element, attrs) {
+
+      element[0].style.position = 'relative';
+      element[0].style.overflow = 'hidden';
+      element[0].style.userSelect = 'none';
+
+      element[0].style.msUserSelect = 'none';
+      element[0].style.mozUserSelect = 'none';
+      element[0].style.webkitUserSelect = 'none';
+
+      function createRipple(evt) {
+        var ripple = angular.element('<span class="gmd-ripple-effect animate">'),
+            rect = element[0].getBoundingClientRect(),
+            radius = Math.max(rect.height, rect.width),
+            left = evt.pageX - rect.left - radius / 2 - document.body.scrollLeft,
+            top = evt.pageY - rect.top - radius / 2 - document.body.scrollTop;
+
+        ripple[0].style.width = ripple[0].style.height = radius + 'px';
+        ripple[0].style.left = left + 'px';
+        ripple[0].style.top = top + 'px';
+        ripple.on('animationend webkitAnimationEnd', function () {
+          angular.element(this).remove();
+        });
+
+        element.append(ripple);
+      }
+
+      element.bind('click', createRipple);
+    }
+  };
+};
+
+exports.default = Component;
+
+},{}],6:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var Component = {
+  require: ['ngModel', 'ngRequired'],
+  bindings: {
+    ngModel: '=',
+    unselect: '@?',
+    options: '<',
+    option: '@',
+    value: '@',
+    placeholder: '@?',
+    onUpdate: "&?"
+  },
+  template: '\n    <div class="dropdown gmd">\n      <label class="control-label floating-dropdown" ng-show="$ctrl.selected" data-ng-bind="$ctrl.placeholder"></label>\n      <button class="btn btn-default gmd dropdown-toggle gmd-select-button"\n              type="button"\n              id="gmdSelect"\n              data-toggle="dropdown"\n              aria-haspopup="true"\n              aria-expanded="true">\n        <span class="item-select" data-ng-show="$ctrl.selected" data-ng-bind="$ctrl.selected"></span>\n        <span data-ng-bind="$ctrl.placeholder" data-ng-hide="$ctrl.selected" class="item-select placeholder"></span>\n        <span class="caret"></span>\n      </button>\n      <ul class="dropdown-menu" aria-labelledby="gmdSelect">\n        <li data-ng-click="$ctrl.clear()" ng-if="$ctrl.unselect">\n          <a data-ng-class="{active: false}">{{$ctrl.unselect}}</a>\n        </li>\n        <li data-ng-repeat="option in $ctrl.options">\n          <a class="select-option" data-ng-click="$ctrl.select(option)" data-ng-bind="option[$ctrl.option] || option" data-ng-class="{active: $ctrl.isActive(option)}"></a>\n        </li>\n      </ul>\n    </div>\n  ',
+  controller: ['$scope', '$attrs', '$timeout', '$element', function ($scope, $attrs, $timeout, $element) {
+    var ctrl = this,
+        ngModelCtrl = $element.controller('ngModel');
+
+    var validate = function validate(input) {
+      var valid = ngModelCtrl.$isEmpty(input);
+      ngModelCtrl.$setValidity('required', valid);
+      return input;
+    };
+    ngModelCtrl.$parsers.unshift(validate);
+    ngModelCtrl.$formatters.push(validate);
+
+    ctrl.selected = ctrl.ngModel;
+
+    $scope.$parent.$watch($attrs.ngModel, function (val, oldVal) {
+      if (val != undefined) {
+        ctrl.selected = val[ctrl.option] || val;
+        ngModelCtrl.$setTouched();
+      }
+    });
+
+    ctrl.isActive = function (option) {
+      return ctrl.selected == (option[ctrl.option] || option);
+    };
+
+    ctrl.select = function (option) {
+      ctrl.selected = option[ctrl.option] || option;
+      ctrl.ngModel = ctrl.value ? option[ctrl.value] : option;
+      if (ctrl.onUpdate) ctrl.onUpdate({ option: option });
+      ngModelCtrl.$setTouched();
+    };
+
+    ctrl.clear = function () {
+      ctrl.ngModel = undefined;
+      ctrl.selected = undefined;
+    };
+  }]
 };
 
 exports.default = Component;
