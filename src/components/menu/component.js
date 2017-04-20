f375a34d43397885543d4077cb655e5702fd246b
Gui Seek <guiseek@gmail.com>
2017-04-20 18:40:34 -0300

    Busca no menu
    Adicionado atributo opcional para busca de itens no menu (recursivo)

--- src/components/menu/component.js
+++ src/components/menu/component.js
@@ -1,16 +1,17 @@
 let Component = {
   bindings: {
     menu: '<',
-    keys: '<'
+    keys: '<',
+    search: '<?'
   },
   template: `
     <ul>
-      <li data-ng-repeat="item in $ctrl.menu" data-ng-show="$ctrl.allow(item)" data-ng-class="{header: item.type == 'header', divider: item.type == 'separator'}">
+      <li data-ng-repeat="item in $ctrl.menu | filter:$ctrl.search" data-ng-show="$ctrl.allow(item)" data-ng-class="{header: item.type == 'header', divider: item.type == 'separator'}">
         <a ng-if="item.type != 'separator'" ui-sref="{{item.state}}">
           <i data-ng-if="item.icon" class="material-icons" data-ng-bind="item.icon"></i>
           <span ng-bind="item.label"></span>
         </a>
-        <gl-menu data-ng-if="item.children.length > 0" menu="item.children" keys="$ctrl.keys"></gl-menu>
+        <gl-menu data-ng-if="item.children.length > 0" menu="item.children" keys="$ctrl.keys" search="$ctrl.search"></gl-menu>
       </li>
     </ul>
   `,
