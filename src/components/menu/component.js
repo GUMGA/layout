2479bdaa17217b44e418c0d05c98b702c568ea50
Gui Seek <guiseek@gmail.com>
2017-04-24 19:03:03 -0300

    Menu mobile step-by-step

--- src/components/menu/component.js
+++ src/components/menu/component.js
@@ -6,19 +6,35 @@ let Component = {
   },
   template: `
     <ul>
+      <li data-ng-show="$ctrl.previous.length > 0" data-ng-click="$ctrl.prev()">
+        <i class="glyphicon glyphicon-chevron-left"></i>
+        <span>Voltar</span>
+      </li>
       <li data-ng-repeat="item in $ctrl.menu | filter:$ctrl.search" data-ng-show="$ctrl.allow(item)" data-ng-class="{header: item.type == 'header', divider: item.type == 'separator'}">
-        <a ng-if="item.type != 'separator'" ui-sref="{{item.state}}">
+        <a ng-if="item.type != 'separator'" ui-sref="{{item.state}}" ng-click="$ctrl.next(item)">
           <i data-ng-if="item.icon" class="material-icons" data-ng-bind="item.icon"></i>
           <span ng-bind="item.label"></span>
         </a>
-        <gl-menu data-ng-if="item.children.length > 0" menu="item.children" keys="$ctrl.keys" search="$ctrl.search"></gl-menu>
       </li>
     </ul>
   `,
   controller: function() {
     let ctrl = this
     ctrl.keys = ctrl.keys || []
+    ctrl.original = angular.copy(ctrl.menu)
+    ctrl.previous = []
+    ctrl.level = 0
 
+    ctrl.prev = () => {
+      ctrl.menu = ctrl.previous.pop()
+    }
+    ctrl.next = item => {
+      if (item.children) {
+        ctrl.search = ''
+        ctrl.previous.push(ctrl.menu)
+        ctrl.menu = item.children
+      }
+    }
     ctrl.allow = item => {
       if (ctrl.keys.length > 0) {
         if (!item.key) return true
