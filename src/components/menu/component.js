dc7a12e98182a31bd928caffd5218d7f0989f8ea
Gui Seek <guiseek@gmail.com>
2017-04-25 18:34:09 -0300

    Menu mobile
    Adicionada navegação horizontal
    Criada área para animações no projeto layout

--- src/components/menu/component.js
+++ src/components/menu/component.js
@@ -6,14 +6,21 @@ let Component = {
   },
   template: `
     <ul>
-      <li data-ng-show="$ctrl.previous.length > 0" data-ng-click="$ctrl.prev()">
-        <i class="glyphicon glyphicon-chevron-left"></i>
-        <span>Voltar</span>
+      <li class="slide-in-right" data-ng-show="$ctrl.previous.length > 0" data-ng-click="$ctrl.prev()">
+        <a>
+          <i class="material-icons">
+            keyboard_arrow_left
+          </i>
+          Voltar
+        </a>
       </li>
-      <li data-ng-repeat="item in $ctrl.menu | filter:$ctrl.search" data-ng-show="$ctrl.allow(item)" data-ng-class="{header: item.type == 'header', divider: item.type == 'separator'}">
+      <li data-ng-repeat="item in $ctrl.menu | filter:$ctrl.search" data-ng-show="$ctrl.allow(item)" data-ng-class="[$ctrl.slide, {header: item.type == 'header', divider: item.type == 'separator'}]">
         <a ng-if="item.type != 'separator'" ui-sref="{{item.state}}" ng-click="$ctrl.next(item)">
           <i data-ng-if="item.icon" class="material-icons" data-ng-bind="item.icon"></i>
           <span ng-bind="item.label"></span>
+          <i data-ng-if="item.children" class="material-icons pull-right">
+            keyboard_arrow_right
+          </i>
         </a>
       </li>
     </ul>
@@ -21,16 +28,15 @@ let Component = {
   controller: function() {
     let ctrl = this
     ctrl.keys = ctrl.keys || []
-    ctrl.original = angular.copy(ctrl.menu)
     ctrl.previous = []
-    ctrl.level = 0
 
     ctrl.prev = () => {
+      ctrl.slide = 'slide-in-left'
       ctrl.menu = ctrl.previous.pop()
     }
     ctrl.next = item => {
       if (item.children) {
-        ctrl.search = ''
+        ctrl.slide = 'slide-in-right'
         ctrl.previous.push(ctrl.menu)
         ctrl.menu = item.children
       }
@@ -40,6 +46,9 @@ let Component = {
         if (!item.key) return true
         return ctrl.keys.indexOf(item.key) > -1
       }
+    }
+    ctrl.$onInit = () => {
+      ctrl.slide = 'slide-in-left'
     }
   }
 }
