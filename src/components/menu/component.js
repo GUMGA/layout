b210accba8a2601ab63d05520e37c2dca0b0cf1e
Gui Seek <guiseek@gmail.com>
2017-04-28 18:14:29 -0300

    Melhorias no menu
    Busca adicionada ao componente
    Opção de voltar com nome do item anterior
    Classe .goback para estilização do item voltar
    Classe level para diferenciação de estilo em níveis do menu, ex: .level0, .level1, .level2.

--- src/components/menu/component.js
+++ src/components/menu/component.js
@@ -1,17 +1,18 @@
 let Component = {
   bindings: {
     menu: '<',
-    keys: '<',
-    search: '<?'
+    keys: '<'
   },
   template: `
-    <ul>
-      <li class="slide-in-right" data-ng-show="$ctrl.previous.length > 0" data-ng-click="$ctrl.prev()">
+    <input type="text" data-ng-model="$ctrl.search" class="form-control gmd" placeholder="Busca...">
+    <div class="bar"></div>
+    <ul data-ng-class="'level'.concat($ctrl.back.length)">
+      <li class="goback slide-in-right" data-ng-show="$ctrl.previous.length > 0" data-ng-click="$ctrl.prev()">
         <a>
           <i class="material-icons">
             keyboard_arrow_left
           </i>
-          Voltar
+          <span data-ng-bind="$ctrl.back[$ctrl.back.length - 1].label"></span>
         </a>
       </li>
       <li data-ng-repeat="item in $ctrl.menu | filter:$ctrl.search" data-ng-show="$ctrl.allow(item)" data-ng-class="[$ctrl.slide, {header: item.type == 'header', divider: item.type == 'separator'}]">
@@ -29,16 +30,19 @@ let Component = {
     let ctrl = this
     ctrl.keys = ctrl.keys || []
     ctrl.previous = []
+    ctrl.back = []
 
     ctrl.prev = () => {
       ctrl.slide = 'slide-in-left'
       ctrl.menu = ctrl.previous.pop()
+      ctrl.back.pop()
     }
     ctrl.next = item => {
       if (item.children) {
         ctrl.slide = 'slide-in-right'
         ctrl.previous.push(ctrl.menu)
         ctrl.menu = item.children
+        ctrl.back.push(item)
       }
     }
     ctrl.allow = item => {
