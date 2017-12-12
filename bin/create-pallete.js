9a99e13023934610b27824e041ed245c7305714b
Mateus Miranda <info.mateusmiranda@gmail.com>
2017-12-12 10:10:12 -0200

    merge

--- bin/create-pallete.js
+++ bin/create-pallete.js
@@ -6,46 +6,55 @@ module.exports = {
             {
                 type: 'input',
                 name: 'darkPrimary',
+                default: '#00796B',
                 message: 'Dark Primary..: '
             },
             {
                 type: 'input',
                 name: 'primary',
+                default: '#009688',
                 message: 'Primary..: '
             },
             {
                 type: 'input',
                 name: 'lightPrimary',
+                default: '#B2DFDB',
                 message: 'Light Primary..: '
             },
             {
                 type: 'input',
                 name: 'textIcons',
+                default: '#FFFFFF',
                 message: 'Text Icons..: '
             },
             {
                 type: 'input',
                 name: 'accent',
+                default: '#607D8B',
                 message: 'Accent..: '
             },
             {
                 type: 'input',
                 name: 'primaryText',
+                default: '#212121',
                 message: 'Primary Text..: '
             },
             {
                 type: 'input',
                 name: 'secundaryText',
+                default: '#757575',
                 message: 'Secundary Text..: '
             },
             {
                 type: 'input',
                 name: 'divider',
+                default: '#EAEAEA',
                 message: 'Divider..: '
             },
             {
                 type: 'input',
                 name: 'background',
+                default: '#ECEFF1',
                 message: 'Background..: '
             }
         ]).then(resp => {
--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,22 @@
-import Menu         from './menu/component.js'
-import MenuShrink         from './menu-shrink/component.js'
-import GmdNotification from './notification/component.js'
-import Select       from './select/component.js'
-import SelectSearch       from './select/search/component.js'
-import Option       from './select/option/component.js'
-import OptionEmpty       from './select/empty/component.js'
-import Input        from './input/component.js'
-import Ripple       from './ripple/component.js'
-import Fab          from './fab/component.js'
-import Spinner      from './spinner/component.js'
-import Hamburger      from './hamburger/component.js'
-import Alert      from './alert/provider.js'
+import Menu         from './menu/component.js';
+import MenuShrink         from './menu-shrink/component.js';
+import GmdNotification from './notification/component.js';
+import Select       from './select/component.js';
+import SelectSearch       from './select/search/component.js';
+import Option       from './select/option/component.js';
+import OptionEmpty       from './select/empty/component.js';
+import Input        from './input/component.js';
+import Ripple       from './ripple/component.js';
+import Fab          from './fab/component.js';
+import Spinner      from './spinner/component.js';
+import Hamburger      from './hamburger/component.js';
+import Alert      from './alert/provider.js';
+import Theme      from './theme/provider.js';
+
 angular
   .module('gumga.layout', [])
   .provider('$gmdAlert', Alert)
+  .provider('$gmdTheme', Theme)
   .directive('gmdRipple', Ripple)
   .component('glMenu', Menu)
   .component('menuShrink', MenuShrink)
